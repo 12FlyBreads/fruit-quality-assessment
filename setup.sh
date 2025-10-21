47f782dacea8ac528406e96a6e001a0b8069c96b
12FlyBreads <arturgsimao@gmail.com>
2025-10-20 23:11:22 -0300

    fix: setup and requirements

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ echo "🍏 Setting up Fruit & Veg Quality environment"
 echo "=============================="
 
 # Define environment name
-VENV_DIR=".fnvq-env"
+VENV_DIR="~/fnvq-env"
 
 # Create virtual environment
 echo "📦 Creating virtual environment..."
-python3 -m venv $VENV_DIR
+python3 -m venv $VENV_DIR --system-site-packages
 
 # Activate venv
 echo "⚙️  Activating virtual environment..."
@@ -30,19 +30,10 @@ DEVICE=$(uname -n)
 echo "🔍 Detected architecture: $ARCH"
 echo "🔍 Device name: $DEVICE"
 
-# Check if running on Raspberry Pi (ARM architecture)
-if [[ "$ARCH" == "armv7l" || "$ARCH" == "aarch64" ]]; then
-    echo "🐍 Installing TensorFlow Lite Runtime for Raspberry Pi..."
-    # Choose version based on architecture
-    if [[ "$ARCH" == "aarch64" ]]; then
-        pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.14.0-cp311-cp311-linux_aarch64.whl
-    else
-        pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.14.0-cp311-cp311-linux_armv7l.whl
-    fi
-else
-    echo "💻 Non-ARM architecture detected. Installing full TensorFlow..."
-    pip install tensorflow
-fi
+echo "📥 Checking Installations..."
+
+pip list | grep -E "(numpy|pillow|opencv|picamera|tflite-runtime)"
+jupyter notebook --generate-config
 
 echo "✅ Environment setup complete!"
 echo "To activate it later, run:"
